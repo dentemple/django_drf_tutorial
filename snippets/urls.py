77f07bf34c11d2a2caeddd847b02107e91728d3d
Denny S. Temple <dev@dentemple.com>
2019-11-06 17:50:45 -0500

    Part 3

--- snippets/urls.py
+++ snippets/urls.py
@@ -7,8 +7,12 @@ from snippets import views
 urlpatterns = [
     # path("snippets/", views.snippet_list),
     # path("snippets/<int:pk>/", views.snippet_detail),
-    url(r"^snippets/<int:pk>", views.snippet_detail),
-    url(r"^snippets", views.snippet_list),
+    # --
+    # url(r"^snippets/<int:pk>", views.snippet_detail),
+    # url(r"^snippets", views.snippet_list),
+    # --
+    url(r"^snippets/<int:pk>", views.SnippetDetail.as_view()),
+    url(r"^snippets", views.SnippetList.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
